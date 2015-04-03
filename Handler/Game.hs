6f38700591deadccf071cb446d4f9e3a914001e8
Jonathan Lange <jml@mumak.net>
2015-04-04 00:54:44 +0100

    Primitive handling of query parameters

--- Handler/Game.hs
+++ Handler/Game.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+
 module Handler.Game where
 
 import Import
@@ -8,7 +13,30 @@ instance ToJSON Game where
   toJSON Game { .. } = object []
 
 
+findGames :: [Game]
+findGames = []
+
+
+data GameStatus = Open | InProgress | Finished | Abandoned deriving (Eq, Show)
+
+textToStatus :: Text -> Maybe GameStatus
+textToStatus "open" = return Open
+textToStatus _ = Nothing
+
+
 getGamesR :: Handler TypedContent
-getGamesR = selectRep $ do
-  provideRep $ defaultLayout $ $(widgetFile "games")
-  provideJson $ ([] :: [Game])
+getGamesR = do
+  status' <- lookupGetParam "status"
+  s <- case status' of
+   Nothing -> do
+     $logInfo "all"
+     return Open
+   Just status'' ->
+     case textToStatus status'' of
+      Nothing -> invalidArgs ["status"]
+      Just status -> do
+        $logInfo (pack (show status))
+        return status
+  selectRep $ do
+    provideRep $ defaultLayout $ $(widgetFile "games")
+    provideJson $ findGames
