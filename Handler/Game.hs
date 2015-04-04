a20a610c9fea61225a2ee7324826652145ff56a5
Jonathan Lange <jml@mumak.net>
2015-04-04 14:43:59 +0100

    Crappy POST handler for new games

--- Handler/Game.hs
+++ Handler/Game.hs
@@ -8,11 +8,29 @@ module Handler.Game where
 import Text.Blaze.Html (ToMarkup, toMarkup)
 import Import
 
-data Game = Game deriving Show
+data GameStatus = Open | InProgress | Finished | Abandoned deriving (Eq, Show)
+
+data BadStatus = BadStatus Text
+
+type Seconds = Int
+
+data PendingGame = OpenG { numPlayers :: Int, turnTimeout :: Seconds } deriving (Eq, Show)
+
+data Game = Pending PendingGame deriving Show
+
 
 instance ToJSON Game where
-  toJSON Game { .. } = object []
+  toJSON _ = object []
 
+
+instance ToJSON PendingGame where
+  toJSON (OpenG { numPlayers = numPlayers,
+                  turnTimeout = turnTimeout }) = object [ "numPlayers" .= numPlayers,
+                                                          "turnTimeout" .= turnTimeout ]
+
+instance FromJSON PendingGame where
+  parseJSON (Object v) = OpenG <$> v .: "numPlayers" <*> v .: "turnTimeout"
+  parseJSON _ = mzero
 
 instance ToMarkup Game where
   toMarkup _ = "nothing to see here"
@@ -26,13 +44,7 @@ allGames = []
 
 
 findGames :: [Game] -> Maybe GameStatus -> [Game]
-findGames _ _ = []
-
-
-data GameStatus = Open | InProgress | Finished | Abandoned deriving (Eq, Show)
-
-
-data BadStatus = BadStatus Text
+findGames gs _ = gs
 
 
 textToStatus :: Text -> Either BadStatus GameStatus
@@ -58,3 +70,11 @@ getGamesR = do
   selectRep $ do
     provideRep $ defaultLayout $ $(widgetFile "games")
     provideJson $ matchingGames
+
+
+postGamesR :: Handler TypedContent
+postGamesR = do
+  newGame <- requireJsonBody :: Handler PendingGame
+  $logInfo (pack $ show newGame)
+  -- XXX: Should use sendResponseCreated, giving route to new game
+  sendResponseStatus status201 ("CREATED" :: Text)
